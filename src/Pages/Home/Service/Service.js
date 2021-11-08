f883b7b0becb696d1bd345acef9c81b16ae1dcf1
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-11-08 17:19:42 +0600

    service  edited

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+
 import Typography from '@mui/material/Typography';
 
 const Service = (props) => {
@@ -20,24 +20,18 @@ const Service = (props) => {
         alt="Paella dish"
       />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
+        
         <Typography variant="h5" component="div">
         {name}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
+      
+        <Typography variant="body2" color="text.secondary">
             {description}
           <br />
           {'"a benevolent smile"'}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+    
     </Card>
           </Grid>
        
