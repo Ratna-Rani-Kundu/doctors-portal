5901214fe5331736032f7e3e10941489f664af3b
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-11-09 15:19:06 +0600

    paper added and btn is working

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import {Grid,Container,Box,Typography} from '@mui/material';
+import Booking from '../Booking/Booking';
 const AvailableAppointments = ({date}) => {
+
+    const bookings=[
+        {
+            id:1,
+            name:'Teeth Orthodonics',
+            time:'08.00 AM - 09.00AM',
+            space:10
+        },
+        {
+            id:2,
+            name:'Cosmetic Dentistry',
+            time:'09.00 AM - 10.00AM',
+            space:8
+        },
+        {
+            id:3,
+            name:'Teeth Cleaning',
+            time:'10.00 AM - 11.00 AM',
+            space:10
+        }
+    ]
     return (
-        <div>
-            <h2>Available appointments on {date?.toDateString()} </h2>
-        </div>
+        <Container>
+            
+            <Typography  sx={{ color: 'info.main',mb:3 }} variant="h5" gutterBottom component="div">Available Appointments on {date?.toDateString()} </Typography>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                  {
+                      bookings.map(booking=><Booking
+                      key={booking.key}
+                      booking={booking}
+                      date={date}
+                      ></Booking>)
+                  }
+               </Grid>
+          </Box>
+        </Container>
     );
 };
 
