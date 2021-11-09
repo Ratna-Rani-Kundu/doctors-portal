8b79b4267234ddc77f48fcbf00a79f048c174e15
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-11-09 13:00:25 +0600

    calendar has benn set

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,22 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
-
-const AppointmentHeader = () => {
+import chair from "../../../images/chair.png"
+import Calendar from '../../Shared/Calendar/Calendar';
+const AppointmentHeader = ({date,setDate}) => {
+  
     return (
-        <div>
-            
-        </div>
+        <Container>
+           <Grid conatiner spacing={2}>
+                <Grid item xs={12}  md={6}>
+                  <Calendar
+                  date={date} setDate={setDate}
+                  ></Calendar>
+                </Grid>
+                <Grid item xs={12}  md={6}>
+                       <img style={{width:'100%'}} src={chair} alt=""/>
+                </Grid>
+            </Grid> 
+        </Container>
     );
 };
 
