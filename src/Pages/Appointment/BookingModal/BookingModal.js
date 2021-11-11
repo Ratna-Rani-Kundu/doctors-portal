a0d00e896f66841949d08918dd9ec166c96ee314
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-11-11 22:17:18 +0600

    modal is updating

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -11,7 +11,7 @@ const BookingModal = ({openBooking,booking,handleBookingClose,date}) => {
 
      const [bookingInfo,SetBookinginfo]=useState({})
       const handleOnBlur=e=>{
-
+           
       }
      const style = {
         position: 'absolute',
